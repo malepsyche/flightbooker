8c04fa6809ee11e991bfcbea5cfba076a41f0404
malepsyche <114489469+malepsyche@users.noreply.github.com>
2025-04-01 22:37:03 +0800

    Enforce Binary UUID as PK

--- booking/schema/booking_db.sql
+++ booking/schema/booking_db.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS booking_db;
 USE booking_db;
 
 CREATE TABLE IF NOT EXISTS booking_table (
-    booking_id INT AUTO_INCREMENT PRIMARY KEY,
+    booking_id BINARY(16) PRIMARY KEY,
     booking_time DATETIME,
     booking_status BOOLEAN,
-    user_id INT,
-    payment_id INT
+    user_id BINARY(16),
+    payment_id BINARY(16)
 )
--- user/schema/user_db.sql
+++ user/schema/user_db.sql
@@ -2,28 +2,9 @@ CREATE DATABASE IF NOT EXISTS user_db;
 USE user_db;
 
 CREATE TABLE IF NOT EXISTS user_table (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id BINARY(16) PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS role_table (
-    role_id INT AUTO_INCREMENT PRIMARY KEY,
-    role_name VARCHAR(255) NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS order_table (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_name VARCHAR(255) NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS user_role_table (
-    user_id INT NOT NULL REFERENCES user_table(user_id),
-    role_id INT NOT NULL REFERENCES role_table(role_id),
-    PRIMARY KEY (user_id, role_id)
-);
-
-CREATE TABLE IF NOT EXISTS user_order_table (
-    user_id INT NOT NULL REFERENCES user_table(user_id),
-    order_id INT NOT NULL REFERENCES order_table(order_id),
-    PRIMARY KEY (user_id, order_id)
-);
