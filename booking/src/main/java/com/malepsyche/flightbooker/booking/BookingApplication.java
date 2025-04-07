6b3bc463dfa7d43fae956631cc513b76b0a59e31
malepsyche <114489469+malepsyche@users.noreply.github.com>
2025-04-08 01:31:21 +0800

    Add MapperScan

--- booking/src/main/java/com/malepsyche/flightbooker/booking/BookingApplication.java
+++ booking/src/main/java/com/malepsyche/flightbooker/booking/BookingApplication.java
@@ -1,9 +1,11 @@
 package com.malepsyche.flightbooker.booking;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.malepsyche.flightbooker.booking.mapper")
 public class BookingApplication {
 
 	public static void main(String[] args) {
